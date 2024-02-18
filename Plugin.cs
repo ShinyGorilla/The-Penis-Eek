f278070647203fae4222740d62bd6efc10b65067
ShinyGorilla <134111158+ShinyGorilla@users.noreply.github.com>
2024-02-18 13:00:09 -0700

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+
+namespace ThePenisEek
+{
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		public static AudioSource Audio;
+		void Start()
+		{
+            GorillaTagger.OnPlayerSpawned(PlayerSpawned);
+        }
+
+		void OnEnable() => HarmonyPatches.ApplyHarmonyPatches();
+		void OnDisable() => HarmonyPatches.RemoveHarmonyPatches();
+
+		void PlayerSpawned()
+		{
+            var bundle = LoadAssetBundle("ThePenisEek.thepeniseek");
+            var asset = bundle.LoadAsset<AudioClip>("The Penis Eek");
+
+            Audio = GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Terrain/SoundPostForest/geo/monkeneedtoswing").GetComponent<AudioSource>();
+			Audio.clip = asset;
+            Debug.Log("ThePenisMod Initialized");
+        }
+
+        public AssetBundle LoadAssetBundle(string path)
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+            AssetBundle bundle = AssetBundle.LoadFromStream(stream);
+            stream.Close();
+            return bundle;
+        }
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace ThePenisEek
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.shinygorilla.gorillatag.thepeniseek";
+		public const string Name = "ThePenisEek";
+		public const string Version = "1.0.0";
+	}
+}
